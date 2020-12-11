096f4a1346f4190c994663fa876a68cdb40f446b
Security Lab <seclab@seclab.nju.edu.cn>
2020-12-12 02:31:34 +0800

    debug mode && this dir

--- ksearch.py
+++ ksearch.py
@@ -3,38 +3,34 @@ import json
 import getopt
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
+this_dir, this_file = os.path.split(os.path.abspath(__file__))
+DEBUG = True
 
 def parse_config():
-
     with open("config.json",'r') as configFile:
         configDict = json.load(configFile)
-
     return configDict
 
 def parse_keywords():
-
     with open("keywords.json",'r') as keywordFile:
         keywordList = json.load(keywordFile)
-
     return keywordList
 
 def grep_keywords(keywordList,RepoDir):
-
     keywordDict = keywordList[0]
     crossDict = keywordList[1]
     targetDict = keywordList[2]
-
-    
 
     # search all the keywords and cross_keywords in the git repo(from config)
     # save results for all keywords and cross-keywords.
         # git log --grep $$ -E > commits/$$.commits
     for key,value in keywordDict.items():
-
         key = key.replace(' ','')
-        print(key)
-        command = "cd " + RepoDir + "&&" + "git log --grep '" + value + "' -E | grep '^commit' > /home/seclab/dingzhu/gitpro/keySearchinGit/commits/" + key + ".commits"
-        os.system(command)
+        # print(key)
+        command = "cd " + RepoDir + "&&" + "git log --grep '" + value + "' -E | grep '^commit' > " + this_dir + "/commits/" + key + ".commits"
+        print(command[command.find("&&")+2:])
+        if not DEBUG:
+            os.system(command)
 
     # cross part
     for key,value in crossDict.items():
@@ -50,23 +46,23 @@ def grep_keywords(keywordList,RepoDir):
             list2 = f2.readlines()
 
         crossPart = list(set(list1)&set(list2))
-
-        with open (outputDir, 'w') as f:
-            f.writelines(crossPart)
+        if not DEBUG:
+            with open (outputDir, 'w') as f:
+                f.writelines(crossPart)
+        else:
+            print(key, " : ", len(crossPart))
     
     # make soft-links for each target keyword,
         # ln -s commits/$$.commits target-commits/$$.commits
-    for key,value in targetDict.items():
-        targetList = value
-    # print(targetList)
-    for target in targetList:
-        target = target.replace(' ','')
-        command = 'ln -sf commits/' + target + '.commits target-commits/' + target + '.commits'
-        os.system(command)
+    if not DEBUG:
+        for key,value in targetDict.items():
+            targetList = value
+        # print(targetList)
+        for target in targetList:
+            target = target.replace(' ','')
+            command = 'ln -sf commits/' + target + '.commits target-commits/' + target + '.commits'
+            os.system(command)
 
-    # show the log numbers of them  
-    # logNub = print_res_of_target_keyword(targetList) 
-    # print(logNub)
     print('oldRepos commits collected')
 
 
@@ -78,8 +74,9 @@ def print_res_of_target_keyword(targetList):
         with open(fileDir , 'r') as f:
             List = f.readlines()
             logNub[target] = len(List)
-    with open("commits/logNub.json" , 'w') as f:
-        json.dump(logNub,f,indent=1)
+    if not DEBUG:
+        with open("commits/logNub.json" , 'w') as f:
+            json.dump(logNub,f,indent=1)
     print('oldcount:',logNub)
     return logNub
     
@@ -100,11 +97,12 @@ def update(keywordList,newRepoDir):
     # search all the keywords and cross_keywords in the git repo(from config)
     # save results for all keywords and cross-keywords.
     for key,value in keywordDict.items():
-
         key = key.replace(' ','')
-        print(key)
-        command = "cd " + RepoDir + "&&" + "git log --grep '" + value + "' -E | grep '^commit' > /home/seclab/dingzhu/gitpro/keySearchinGit/update/commits/" + key + ".commits"
-        os.system(command)
+        # print(key)
+        command = "cd " + RepoDir + "&&" + "git log --grep '" + value + "' -E | grep '^commit' > " + this_dir + "/commits/" + key + ".commits"
+        print(command[command.find("&&")+2:])
+        if not DEBUG:
+            os.system(command)
 
     # cross part
     for key,value in crossDict.items():
@@ -121,18 +119,22 @@ def update(keywordList,newRepoDir):
 
         crossPart = list(set(list1)&set(list2))
 
-        with open (outputDir, 'w') as f:
-            f.writelines(crossPart)
+        if not DEBUG:
+            with open (outputDir, 'w') as f:
+                f.writelines(crossPart)
+        else:
+            print(key, " : ", len(crossPart))
     
     # make soft-links for each target keyword,
     # ln -s commits/$$.commits target-commits/$$.commits
     for key,value in targetDict.items():
         targetList = value
-    # print(targetList)
-    for target in targetList:
-        target = target.replace(' ','')
-        command = 'ln -sf update/commits/' + target + '.commits update/target-commits/' + target + '.commits'
-        os.system(command)
+        # print(targetList)
+    if not DEBUG:
+        for target in targetList:
+            target = target.replace(' ','')
+            command = 'ln -sf update/commits/' + target + '.commits update/target-commits/' + target + '.commits'
+            os.system(command)
 
     new_commit_count(targetList)
     print('newRepos commits collected')
@@ -154,16 +156,18 @@ def new_commit_count(targetList):
             oldlist = oldf.readlines()
         deflist = list(set(newlist)-set(oldlist))
         # store new commits
-        with open (outputDir,'w') as f:
-            f.writelines(deflist)
+        if not DEBUG:
+            with open (outputDir,'w') as f:
+                f.writelines(deflist)
         difcount[target] = len(newlist) - len(oldlist)
         allcount[target] = len(newlist)
     # store the count for new commmits
-    with open ("update/newcommits/difcount.json",'w') as f:
-        json.dump(difcount,f,indent=1)
-    # store all nubs of commits in new repo
-    with open ("update/commits/allcount.json",'w') as f:
-        json.dump(allcount,f,indent=1)
+    if not DEBUG:
+        with open ("update/newcommits/difcount.json",'w') as f:
+            json.dump(difcount,f,indent=1)
+        # store all nubs of commits in new repo
+        with open ("update/commits/allcount.json",'w') as f:
+            json.dump(allcount,f,indent=1)
     print('newcount:',allcount)
     print('difcount:',difcount)
 
@@ -178,7 +182,6 @@ def main():
     configDict = parse_config()
     keywordList = parse_keywords()
 
-
     oldRepos = configDict['RepoDir']
     newRepos = args.newRepo
     print('oldRepos:',oldRepos)
@@ -188,7 +191,7 @@ def main():
 
     update(keywordList,newRepos)
 
-    # colledt targetkeywords and count them
+    # collect targetkeywords and count them
     targetDict = keywordList[2]
     for key,value in targetDict.items():
         targetList = value
